94c91137e216e6e9c30310293cb9e49ce381d1a9
Manuel Duarte <carlosd99m@gmail.com>
2021-10-04 16:52:42 -0600

    feat: enviando info al socket

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
@@ -9,8 +10,22 @@ namespace SocketClient
         static void Main(string[] args)
         {
             Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
             IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+
+            listen.Connect(connect);
+
+            byte[] send_info = new byte[100];
+            string data; 
+
+            Console.WriteLine("Ingrese la info a enviar");
+            data = Console.ReadLine();
+
+            send_info = Encoding.Default.GetBytes(data);
+
+            listen.Send(send_info);
+
+            Console.ReadKey();
+
         }
     }
 }
