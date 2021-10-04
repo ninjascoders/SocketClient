aeffe33431801a79541e2cb60476f4aac5f60b95
Manuel Duarte <carlosd99m@gmail.com>
2021-10-04 16:31:37 -0600

    Add project files.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+        }
+    }
+}
